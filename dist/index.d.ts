1f1ba1887dce7e662723e32ac94ca26d0a78361a
Oscar Bonelli <bonelli.personal@gmail.com>
2025-09-17 01:31:47 -0600

    feature: incorporating UI elements v5

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,17 +1,15 @@
-import * as react_jsx_runtime from 'react/jsx-runtime';
-import * as React from 'react';
+import React from 'react';
 
-type Variant = 'primary' | 'secondary' | 'ghost';
-type Size = 'sm' | 'md' | 'lg';
+type ButtonVariant = 'primary' | 'outline' | 'link' | 'outlineFill';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    variant?: Variant;
-    size?: Size;
-    loading?: boolean;
+    /** Visual style of the button */
+    variant?: ButtonVariant;
 }
-declare function Button({ className, variant, size, loading, children, ...props }: ButtonProps): react_jsx_runtime.JSX.Element;
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+    label?: string;
+    error?: boolean;
+    onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
-declare function Input({ className, ...props }: InputProps): react_jsx_runtime.JSX.Element;
 
-export { Button, type ButtonProps, Input, type InputProps };
+export type { ButtonProps, InputProps };
