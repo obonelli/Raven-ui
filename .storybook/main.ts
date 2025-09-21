13a27a683714f1f0a67f603114624488ac266b79
Oscar Bonelli <bonelli.personal@gmail.com>
2025-09-20 21:55:14 -0600

    feature: v6

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,9 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
-import { mergeConfig } from 'vite';
-import tailwind from '@tailwindcss/vite';
 
 const config: StorybookConfig = {
   framework: { name: '@storybook/nextjs-vite', options: {} },
   addons: ['@storybook/addon-docs'],
-  stories: [
-    // Solo buscamos stories dentro de src/components
-    '../src/components/**/*.stories.@(ts|tsx|mdx)',
-  ],
-  async viteFinal(baseConfig) {
-    return mergeConfig(baseConfig, {
-      plugins: [tailwind()],
-    });
-  },
+  stories: ['../src/components/**/*.stories.@(ts|tsx|mdx)'],
 };
 
 export default config;
